7e3a77d6b86449757250701b3aa0c0524ce49512
Jens Kutilek <webmail@kutilek.de>
2020-06-07 22:41:56 +0200

    Make regexes global

--- SmartTypography/smart.js
+++ SmartTypography/smart.js
@@ -1,5 +1,21 @@
 /* jshint esversion: 6 */
 
+const singleQuoteRegex = /'/g
+const doubleQuoteRegex = /"/g
+
+const openDoubleQuoteRegex = /(\s|^|\()"/gi
+const openSingleQuoteRegex = /(\s|^|\()'/gi
+const closeDoubleQuoteRegex1 = /(\w|[!?.,]|'|\))"/gi
+const closeDoubleQuoteRegex2 = /"[$,]/gi
+const closeSingleQuoteRegex1 = /(\w|"|\))'/gi
+const closeSingleQuoteRegex2 = /'[$,]/gi
+const enDashRegex = /( - |--)/gi
+
+const apostropheRegex1 = /(\w)['´`](\w)['´`](\w)/gi
+const apostropheRegex2 = /(\w)['´`](\w)/gi
+const co2Regex = /CO2/g
+const h2oRegex = /H2O/g
+
 let openDoubleQuote, openSingleQuote, closeDoubleQuote, closeSingleQuote, enDash
 
 function findLanguage (el) {
@@ -36,35 +52,35 @@ function findAndReplace (elements) {
 }
 
 function replaceOpenDoubleQuote (text, char) {
-  return text.replace(/(\s|^|\()"/gi, `$1${char}`)
+  return text.replace(openDoubleQuoteRegex, `$1${char}`)
 }
 
 function replaceOpenSingleQuote (text, char) {
-  return text.replace(/(\s|^|\()'/gi, `$1${char}`)
+  return text.replace(openSingleQuoteRegex, `$1${char}`)
 }
 
 function replaceCloseDoubleQuote (text, char) {
-  text = text.replace(/(\w|[!?.,]|'|\))"/gi, `$1${char}`)
-  return text.replace(/"[$,]/gi, char)
+  text = text.replace(closeDoubleQuoteRegex1, `$1${char}`)
+  return text.replace(closeDoubleQuoteRegex2, char)
 }
 
 function replaceCloseSingleQuote (text, char) {
-  text = text.replace(/(\w|"|\))'/gi, `$1${char}`)
-  return text.replace(/'[$,]/gi, char)
+  text = text.replace(closeSingleQuoteRegex1, `$1${char}`)
+  return text.replace(closeSingleQuoteRegex2, char)
 }
 
 function replaceEnDash (text, char) {
-  return text.replace(/ - /gi, char)
+  return text.replace(enDashRegex, char)
 }
 
 function replaceText (lang, node) {
   let value = node.nodeValue
   if (value) {
     // apostrophe is the same for any language
-    value = value.replace(/(\w)['´`](\w)['´`](\w)/gi, '$1’$2’$3')
-    value = value.replace(/(\w)['´`](\w)/gi, '$1’$2')
-    value = value.replace(/CO2/g, 'CO₂')
-    value = value.replace(/H2O/g, 'H₂O')
+    value = value.replace(apostropheRegex1, '$1’$2’$3')
+    value = value.replace(apostropheRegex2, '$1’$2')
+    value = value.replace(co2Regex, 'CO₂')
+    value = value.replace(h2oRegex, 'H₂O')
 
     switch (lang) {
       case 'de':
